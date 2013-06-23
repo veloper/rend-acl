b0e0a1e0039554622cc88184ebb1bcf1ca732ea2
Daniel Doezema <dan@doezema.com>
2013-06-23 12:43:13 -0400

    Adding minitest/autorun prior to turn/autorun

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,7 @@ require 'support/passing_assertion'
 require 'support/failing_assertion'
 
 # Auto run tests with Turn gem.
+require "minitest/autorun"
 require "turn/autorun"
 
 # https://coveralls.io Integration
