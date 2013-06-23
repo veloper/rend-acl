dcef8a4fb69b7a0bd8e5f6c30784b19aa976bef7
Daniel Doezema <dan@doezema.com>
2013-06-23 12:11:44 -0400

    Fixing syntax oddity in a test.

--- test/test_wiki_pages_resources.rb
+++ test/test_wiki_pages_resources.rb
@@ -11,7 +11,7 @@ class WikiPagesResourcesTest < MiniTest::Unit::TestCase
       building_resource = Rend::Acl::Resource.new("building")
 
       @acl.add_resource!(city_resource)
-      @acl.add_resource!(building_resource, city_resource) # Inheritance from city_resource
+      @acl.add_resource!(building_resource, city_resource)
 
       mayor_role = Rend::Acl::Role.new("mayor")
       @acl.add_role!(mayor_role)
@@ -19,7 +19,7 @@ class WikiPagesResourcesTest < MiniTest::Unit::TestCase
       @acl.allow! :role => mayor_role, :resource => city_resource
 
       assert_equal true, @acl.allowed?(:role => mayor_role, :resource => city_resource)
-      assert_equal true, (@acl.allowed? :role => mayor_role, :resource => building_resource)
+      assert_equal true, @acl.allowed?(:role => mayor_role, :resource => building_resource)
     end
 
 end
