645a97f293ca5c1bf92bc477c3574e8f07d946ec
Daniel Doezema <dan@doezema.com>
2013-06-23 13:12:36 -0400

    Making the turn gem degrade gracefully on Travis CI.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,11 +6,13 @@ require 'support/mock_assertion'
 require 'support/passing_assertion'
 require 'support/failing_assertion'
 
-# Auto run tests=
+# Autorun tests
 require "minitest/autorun"
 
-# Optionally try to use TURN gem
-begin; require 'turn/autorun'; rescue LoadError; end
+# Optionally try to use TURN gem if we're in a RUBY_VERSION >= 1.9.3 environment
+if Gem::Version.new(RUBY_VERSION) > Gem::Version.new('1.9.2')
+  begin; require 'turn/autorun'; rescue LoadError; end
+end
 
 # https://coveralls.io Integration
 require 'coveralls'
